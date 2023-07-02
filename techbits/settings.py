74ae965434c345e6656fc8973cf3dadc4e939a99
Delemangi <milev.stefan@gmail.com>
2023-07-02 15:07:43 +0200

    Enable debug mode

--- techbits/settings.py
+++ techbits/settings.py
@@ -28,14 +28,9 @@ SECRET_KEY = (
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = [
-    "Delemangi.pythonanywhere.com",
-    "localhost",
-    "127.0.0.1",
-    ".vercel.app",
-]
+ALLOWED_HOSTS: list[str] = []
 
 
 # Application definition
